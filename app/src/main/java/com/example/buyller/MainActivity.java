0cb82009bdc6f59bb1029f4833b0fd925eadfd97
Nothing00980 <yuvrajbhati00980@gmail.com>
2023-05-03 19:26:15 +0530

    more addition

--- app/src/main/java/com/example/buyller/MainActivity.java
+++ app/src/main/java/com/example/buyller/MainActivity.java
@@ -4,19 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.filament.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    public void buy(View v){
+    public void buy(View view){
+        Toast.makeText(this, "buy page clicked", Toast.LENGTH_SHORT).show();
+//        Intent intent = new Intent(this,buypage.class);
+//        startActivity(intent);
 
     }
     public void sell(View v){
@@ -31,4 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
 }
