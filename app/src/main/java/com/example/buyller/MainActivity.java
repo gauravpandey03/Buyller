d453d72628856b0278ac3e814417a10290943a49
Nothing00980 <yuvrajbhati00980@gmail.com>
2023-05-05 12:22:54 +0530

    changes

--- app/src/main/java/com/example/buyller/MainActivity.java
+++ app/src/main/java/com/example/buyller/MainActivity.java
@@ -4,34 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.filament.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    public void buy(View view){
-        Toast.makeText(this, "buy page clicked", Toast.LENGTH_SHORT).show();
-//        Intent intent = new Intent(this,buypage.class);
-//        startActivity(intent);
-
-    }
-    public void sell(View v){
-        Intent intent = new Intent(this,sellpage.class);
-        startActivity(intent);
-    }
-    public void aboutus(View v){
-        Intent intent = new Intent(this,aboutpage.class);
-        startActivity(intent);
-    }
-    public void query(View v){
+ImageView img1;
+ImageView img2;
+ImageView img3;
+ImageView img4;
 
 
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img1 = findViewById(R.id.extdes1);
+        img4 = findViewById(R.id.extdes3);
+        img3 = findViewById(R.id.extdes4);
+
+        img2 = findViewById(R.id.extdes2);
+
+
+
+    }
+
+    public void buy(View view){
+//        Toast.makeText(this, "buy page clicked", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this,buypage.class);
+        startActivity(intent);
+
+    }
+    public void sell(View view){
+
+        Intent intent = new Intent(this,sellpage.class);
+        startActivity(intent);
+    }
+    public void aboutus(View view){
+
+        Intent intent = new Intent(this,aboutpage.class);
+        startActivity(intent);
+    }
+    public void query(View view){
+        Toast.makeText(this, "this is query class", Toast.LENGTH_SHORT).show();
+
     }
 
 
