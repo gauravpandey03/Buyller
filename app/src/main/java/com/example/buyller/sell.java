fc0d42b9d9ed09337e23da615fbdef8fc21d1e9f
Manohar123261 <121283980+Manohar123261@users.noreply.github.com>
2023-05-15 15:57:21 +0530

    image picker is added

--- app/src/main/java/com/example/buyller/sell.java
+++ app/src/main/java/com/example/buyller/sell.java
@@ -1,23 +1,30 @@
 package com.example.buyller;
 
+
+import static com.github.drjacky.imagepicker.ImagePicker.*;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.github.drjacky.imagepicker.ImagePicker;
+import com.github.drjacky.imagepicker.constant.ImageProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class sell extends AppCompatActivity {
     EditText pname,pdetails,price,sdetails,snumber;
-    ImageView backarow;
-    Button datainsert;
-
+    ImageView backarow,imgpkr;
+    Button datainsert,p;
+    private final int ReQUEST_CODE=1000;
     DatabaseReference buyllerdata;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class sell extends AppCompatActivity {
         sdetails = findViewById(R.id.sdetailsentry);
         snumber = findViewById(R.id.sellerpentry);
         datainsert = findViewById(R.id.databasesubmit);
+        imgpkr=findViewById(R.id.imgpkr);
         // data base name
         buyllerdata = FirebaseDatabase.getInstance().getReference().child("maindata");
         datainsert.setOnClickListener(new View.OnClickListener() {
@@ -38,8 +46,28 @@ public class sell extends AppCompatActivity {
              insertdata();
             }
         });
+        imgpkr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        Intent igal=new Intent(Intent.ACTION_PICK);
+                        igal.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(igal,ReQUEST_CODE);
+
+            }
+        });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+        if(requestCode==ReQUEST_CODE){
+           imgpkr.setImageURI(data.getData());
+        }
+        }
+    }
+
     private  void insertdata(){
         String prouductname = pname.getText().toString();
         String prodetails = pdetails.getText().toString();
