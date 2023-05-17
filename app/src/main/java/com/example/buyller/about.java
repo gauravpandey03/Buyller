f6502d43e7002626f065b6ee28266a1165c015bd
gauravpandey03 <gauravpandey10082003@gmail.com>
2023-05-17 14:43:59 +0530

    emai functioning added

--- app/src/main/java/com/example/buyller/about.java
+++ app/src/main/java/com/example/buyller/about.java
@@ -11,22 +11,32 @@ import android.widget.ImageView;
 
 public class about extends AppCompatActivity {
     ImageView backarow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        backarow=findViewById(R.id.buyback);
+        backarow = findViewById(R.id.buyback);
     }
-    public void arrowback(View view){
-        Intent intent = new Intent(this,MainActivity.class);
+
+    public void arrowback(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
     public void makeCall(View view) {
-        String phoneNumber = "6387664293"; // Replace with the desired phone number
+        String phoneNumber = "6387664293";
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:" + phoneNumber));
         startActivity(intent);
     }
 
+    public void composeEmail(View view) {
+        String recipient = "buyller@gmail.com";
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:" + recipient));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Registration");
+
+        startActivity(Intent.createChooser(intent, "Compose Email"));
+    }
 }
